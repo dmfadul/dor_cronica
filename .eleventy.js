73dee2b0d77df6c9f53c29011cbc7611e2393213
doctormatterhorn <dmf030@gmail.com>
2025-09-08 21:11:29 -0300

    trying to fix bug. returned to original

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,11 @@
 module.exports = function (eleventyConfig) {
-  // Pass-through for your global stylesheet and any future assets
   eleventyConfig.addPassthroughCopy("styles.css");
-  //eleventyConfig.addPassthroughCopy({"assets": "assets"}); // create later if needed
 
   return {
     dir: {
-      input: ".",           // project root
+      input: ".",
       includes: "_includes",
-      //data: "_data",        // optional (only if you add a _data dir)
       output: "_site"
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "html"]
+    }
   };
 };
